cd16acb21d02b4182983767326ed3e93620c1232
Wesley Araujo <wesleyfaraujjo@gmail.com>
2021-06-09 18:58:36 -0300

    fix

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('./app')
 
 require('dotenv').config()
+const port = process.env.PORT || 3333
 
-app.listen(process.env.PORT || 3333, console.log(`Server running on port ${process.env.PORT}`))
+app.listen(port, console.log(`Server running on port ${port}`))
